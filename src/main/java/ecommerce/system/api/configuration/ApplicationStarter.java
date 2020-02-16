2427b7a61ac068b06d7482cb1568d8753eba0d80
ArturRSO <artur.rso@outlook.com>
2020-02-16 16:58:29 -0300

    Initial structure

---
+++ src/main/java/ecommerce/system/api/configuration/ApplicationStarter.java
@@ -0,0 +1,12 @@
+package ecommerce.system.api.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationStarter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationStarter.class, args);
+	}
+}
---
+++ src/test/java/ecommerce/system/api/configuration/ApplicationStarterTests.java
@@ -0,0 +1,12 @@
+package ecommerce.system.api.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationStarterTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
