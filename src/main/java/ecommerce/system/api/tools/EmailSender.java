757300eda7d7d4df2dd6af0942b2fb785f19927e
Artur Rios <arturdev@duck.com>
2023-05-27 00:37:49 -0300

    fix: add sender email

--- src/main/java/ecommerce/system/api/tools/EmailSender.java
+++ src/main/java/ecommerce/system/api/tools/EmailSender.java
@@ -2,6 +2,7 @@ package ecommerce.system.api.tools;
 
 import ecommerce.system.api.models.SimpleMailModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -13,6 +14,9 @@ import javax.mail.internet.MimeMessage;
 @Component
 public class EmailSender {
 
+    @Value("${spring.mail.username}")
+    private String from;
+
     private final JavaMailSender emailSender;
 
     @Autowired
@@ -23,9 +27,12 @@ public class EmailSender {
     public void sendSimpleMail(SimpleMailModel mail) {
 
         SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom(from);
         message.setTo(mail.getTo());
         message.setSubject(mail.getSubject());
         message.setText(mail.getText());
+
         this.emailSender.send(message);
     }
 
@@ -34,9 +41,10 @@ public class EmailSender {
         MimeMessage mimeMessage = this.emailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
 
-        helper.setText(mail.getText(), true);
+        helper.setFrom(from);
         helper.setTo(mail.getTo());
         helper.setSubject(mail.getSubject());
+        helper.setText(mail.getText(), true);
 
         this.emailSender.send(mimeMessage);
     }
